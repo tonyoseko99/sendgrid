2478358209e46c0bc4033f33bb549c980ad8a549
Tonny Oseko <tonnyoseko0@gmail.com>
2022-12-08 22:59:20 +0300

    function to handke submit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,31 @@
-import react from 'react';
-import { Mail } from '@sendgrid/mail';
+import react from "react";
+import { Mail } from "@sendgrid/mail";
 
 const Home = () => {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
     const msg = {
-        to: 'recipient@example.com',
-        from: 'sender@example.com',
-        subject: 'Hello world',
-        text: 'This is a test email from Sendgrid in a React app.',
-      };
-      
-}
+      to: "recipient@example.com",
+      from: "sender@example.com",
+      subject: "Hello world",
+      text: "This is a test email from Sendgrid in a React app.",
+    };
+
+    //   set up your Sendgrid API key as an environment variable
+    const sendEmail = () => {
+      Mail.setApiKey(process.env.SENDGRID_API_KEY);
+      Mail.send(msg);
+    };
+
+    sendEmail();
+  };
+
+  return (
+    <>
+      <h5>Sendgrid Email</h5>
+      <p>let's send some emails to random people </p>
+    </>
+  );
+};
 
 export default Home;
